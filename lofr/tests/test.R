68e7eec004b76a1a9261d2196287f5ea6b898626
5hun <niki_junpei@hotmail.com>
2016-09-03 21:49:47 +0900

    no message

--- lofr/tests/test.R
+++ lofr/tests/test.R
@@ -1,5 +1,4 @@
 library(RUnit)
 library(lofr)
-test.suite <- defineTestSuite("all", 
-    dirs=file.path(getwd(), "runit"))
+test.suite <- defineTestSuite("all", dirs=file.path(getwd(), "runit"))
 runTestSuite(test.suite)
